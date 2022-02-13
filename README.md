5ef55551cc57e6058d6756965a69a3fd5c152ff5
Jan Biehl <jpbiehl@googlemail.com>
2022-02-13 11:35:33 +0100

    test mqtt code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-AquariumDosierpumpe
+# AquariumDosierpumpe
+
+## Resources
+
+- https://docs.espressif.com/projects/esp-idf/en/latest/esp32s2/
--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@
 #include "esp_netif.h"
 #include "esp_smartconfig.h"
 
+#include "mqtt_client.h"
+
 #include "WebServer.h"
 #include "esp_smartconfig.h"
 
@@ -23,15 +25,17 @@ static EventGroupHandle_t s_wifi_event_group;
    to the AP with an IP? */
 static const int CONNECTED_BIT = BIT0;
 static const int ESPTOUCH_DONE_BIT = BIT1;
-static const char *TAG = "smartconfig_example";
+static const char *TAG = "Dosierpumpe";
 
-static void smartconfig_example_task(void * parm);
+static uint8_t* mac_address;
 
-static void event_handler(void* arg, esp_event_base_t event_base,
+static void smartconfig_task(void * parm);
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
 {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        xTaskCreate(smartconfig_example_task, "smartconfig_example_task", 4096, NULL, 3, NULL);
+        xTaskCreate(smartconfig_task, "smartconfig_task", 4096, NULL, 3, NULL);
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
         esp_wifi_connect();
         xEventGroupClearBits(s_wifi_event_group, CONNECTED_BIT);
@@ -79,7 +83,64 @@ static void event_handler(void* arg, esp_event_base_t event_base,
     }
 }
 
-static void smartconfig_example_task(void * parm)
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+    esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t) event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+
+    switch ((esp_mqtt_event_id_t) event_id)
+    {
+    case MQTT_EVENT_BEFORE_CONNECT:
+        ESP_LOGI(TAG, "MQTT before connect event");
+        break;
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT connected event");
+
+        // here we got a connection with a broker, we can start listening for something
+        msg_id = esp_mqtt_client_subscribe(client, "homeassistant/switch/ABDCE", 0);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT disconnected event");
+        break;
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT unsubscribed event");
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT published event");
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT data event");
+
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        if (strncmp(event->data, "send binary please", event->data_len) == 0) {
+            ESP_LOGI(TAG, "Sending the binary");
+        }
+
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT error event");
+        break;
+    case MQTT_EVENT_DELETED:
+        ESP_LOGI(TAG, "MQTT deleted event");
+        break;  
+
+    default: 
+        ESP_LOGI(TAG, "Unhandeled mqtt event");
+        break;
+    }
+}
+
+static void smartconfig_task(void * parm)
 {
     EventBits_t uxBits;
     ESP_ERROR_CHECK( esp_smartconfig_set_type(SC_TYPE_ESPTOUCH) );
@@ -101,27 +162,47 @@ static void smartconfig_example_task(void * parm)
 WebServer* _webServer = new WebServer();
 
 void initializeWifi() {
-    ESP_ERROR_CHECK(esp_netif_init());
     s_wifi_event_group = xEventGroupCreate();
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_t* sta_netif = esp_netif_create_default_wifi_sta();
     assert(sta_netif);
 
     wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&config));
 
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(SC_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(SC_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
 
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_start());
 }
 
+void initializeMqtt() {
+    // TODO: remove this literals
+    esp_mqtt_client_config_t mqtt_config = esp_mqtt_client_config_t();
+    mqtt_config.uri = "http://192.168.10.56:8883";
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_handle_t(&mqtt_config);
+    esp_mqtt_client_register_event(client, MQTT_EVENT_ANY, &mqtt_event_handler, NULL);
+
+    ESP_ERROR_CHECK(esp_mqtt_client_start(client));
+}
+
 extern "C" void app_main() {
+    // read and store the mac address
+    esp_efuse_mac_get_default(mac_address);
+
     ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
 
     initializeWifi();
+    initializeMqtt();
+
+    //configureMqttAutodiscover();
+    
 
     //_webServer->startWebserver(nullptr);
 
